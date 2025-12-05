5113f0d1fdbb0360c595469266326b0c8c67e0fa
JGilmore1029 <Skinny_Gilmore@hotmail.com>
2025-12-05 08:46:56 -0600

    added changes to client REPL

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -40,9 +38,43 @@ func main() {
 	}
 	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
 
-	// wait for ctrl+c
+	newstate := gamelogic.NewGameState(username)
+
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+		switch input[0] {
+		case "spawn":
+			if err := newstate.CommandSpawn(input); err != nil {
+				fmt.Println("Error:", err)
+			}
+		case "move":
+			_, err := newstate.CommandMove(input)
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+		case "status":
+			newstate.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "quit":
+			fmt.Println("🛑 Shutting down server...")
+			return
+
+		default:
+			fmt.Println("Unknown Command")
+			continue
+		}
+	}
+
+	/* wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
 	fmt.Println("RabbitMQ connection closed.")
+	*/
 }
